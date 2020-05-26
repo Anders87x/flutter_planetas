c0ecb43b81f3cd82a18707c68fffd986d23ddfee
Anderson <davis_anderson_87@hotmail.com>
2020-05-25 20:52:57 -0500

    25/05/2020 Subiendo a GitHub

---
+++ lib/data.dart
@@ -0,0 +1,73 @@
+class PlanetInfo {
+  final int position;
+  final String name;
+  final String iconImage;
+  final String description;
+  final List<String> images;
+
+  PlanetInfo(
+    this.position, {
+    this.name,
+    this.iconImage,
+    this.description,
+    this.images,
+  });
+}
+
+List<PlanetInfo> planets = [
+  PlanetInfo(1,
+      name: 'Mercurio',
+      iconImage: 'assets/mercury.png',
+      description:
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+      images: [
+        'https://cdn.pixabay.com/photo/2013/07/18/10/57/mercury-163610_1280.jpg',
+        'https://cdn.pixabay.com/photo/2014/07/01/11/38/planet-381127_1280.jpg',
+        'https://cdn.pixabay.com/photo/2015/06/26/18/48/mercury-822825_1280.png',
+        'https://image.shutterstock.com/image-illustration/mercury-high-resolution-images-presents-600w-367615301.jpg'
+      ]),
+  PlanetInfo(2,
+      name: 'Venus',
+      iconImage: 'assets/venus.png',
+      description:
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",      images: [
+        'https://cdn.pixabay.com/photo/2011/12/13/14/39/venus-11022_1280.jpg',
+        'https://image.shutterstock.com/image-photo/solar-system-venus-second-planet-600w-515581927.jpg'
+      ]),
+  PlanetInfo(3,
+      name: 'Tierra',
+      iconImage: 'assets/earth.png',
+      description:
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",      images: [
+        'https://cdn.pixabay.com/photo/2011/12/13/14/31/earth-11015_1280.jpg',
+        'https://cdn.pixabay.com/photo/2011/12/14/12/11/astronaut-11080_1280.jpg',
+        'https://cdn.pixabay.com/photo/2016/01/19/17/29/earth-1149733_1280.jpg',
+        'https://image.shutterstock.com/image-photo/3d-render-planet-earth-viewed-600w-1069251782.jpg'
+      ]),
+  PlanetInfo(4,
+      name: 'Marte',
+      iconImage: 'assets/mars.png',
+      description:
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",      images: []),
+  PlanetInfo(5,
+      name: 'Jupiter',
+      iconImage: 'assets/jupiter.png',
+      description:
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+      images: []),
+  PlanetInfo(6,
+      name: 'Saturno',
+      iconImage: 'assets/saturn.png',
+      description:
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",      images: []),
+  PlanetInfo(7,
+      name: 'Urano',
+      iconImage: 'assets/uranus.png',
+      description:
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",      images: []),
+  PlanetInfo(8,
+      name: 'Neptuno',
+      iconImage: 'assets/neptune.png',
+      description:
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",      images: []),
+];
